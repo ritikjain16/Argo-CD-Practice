57e8677605fbcfed5330aaf9b183177abd1c78f7
Ritik Jain <ritik9628@gmail.com>
2025-10-17 20:04:47 +0530

    ui-approach-added

---
+++ 01_first_app_deployment/ui_approach/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 6000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    msg: "Hello, This is Node.js Argo CD UI Approach Application",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
